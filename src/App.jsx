75b13a9b588cdf9ca1c2ff78ca7cf73272783cbf
umairasim0598 <26908074-umairasim0598@users.noreply.replit.com>
2025-02-09 12:20:12 +0000

    User checkpoint: Enhance portfolio website design and add social media links.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
-
 import "./index.css";
 import "./App.css";
 import Typewriter from "react-typewriter-effect";
 import Navbar from "./components/Navbar";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { TbBrandFiverr } from "react-icons/tb";
 
 export default function App() {
   return (
-    <div className="bg-black h-[100vh] text-white ">
+    <div className="bg-black text-white font-poppins ">
       <Navbar />
-      <main>
-        <div className="m-4 flex h-[40vh]">
-          <div className="h-full flex justify-center items-center p-4">
-            <div className="text-4xl font-bold">
-              <Typewriter
-                multiText={[
-                  "Hi, I'm Umair! 👋",
-                  "A Passionate Web Developer 🚀",
-                  "Building Scalable Web Apps 💻",
-                  "Creating Interactive User Experiences 🎨",
-                  "Let's Build Something Amazing Together! ✨",
-                ]}
-                multiTextLoop={true}
-                cursorColor="#3498DB"
-                typeSpeed={100}
-                deleteSpeed={50}
-                delay={2000}
-              />
+      <main className="p-4">
+        {/* Upper div */}
+        <div className="upper flex justify-between items-center bdr upper-height p-4">
+          {/* Type writer  */}
+          <div className="typewriter mt-6 w-[60vw]">
+            <div className="custom-shadow text-5xl font-bold ">
+            <Typewriter
+              multiText={[
+                "Hi, I'm Umair Asim!",
+                "A Passionate Web Developer ",
+                "Building Scalable Web Apps ",
+                "Creating Interactive User Experiences ",
+                "Let's Build Something Amazing Together! ",
+              ]}
+              multiTextLoop={true}
+              cursorColor="#3498DB"
+              typeSpeed={100}
+              deleteSpeed={50}
+              delay={2000}
+            />
+            </div>
+            {/* Paragraph under typewriter  */}
+            <div className="mt-10">
+              <p>
+                Hi, I'm Umair! A passionate web developer skilled in React,
+                Next.js, Express, and Node.js. I love building modern,
+                user-friendly, and efficient web applications. Always eager to
+                learn new technologies and improve my skills.
+              </p>
+              {/* Social Icons */}
+              <div className="socialIcons my-3 flex space-x-2">
+                <div className="border-2 border-[#3498DB] rounded-full p-2 w-fit">
+                <FaLinkedinIn />
+                </div>
+                <div className="border-2 border-[#3498DB] rounded-full p-2 w-fit">
+                  <TbBrandFiverr />
+                </div>
+              </div>
             </div>
           </div>
-          <div>other block image</div>
+          <div className="image bdr">IMAGE</div>
+        </div>
+        <div className="About">
+          About
         </div>
       </main>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,32 @@ export default function Navbar() {
     <nav className="px-8 py-4">
       <div className="flex justify-between items-center">
         <div className="text-white text-2xl font-bold">
-          <a href="/">Logo</a>
+          <a className="transition duration-200  hover:drop-shadow-[0_0_10px_#3498DB]" href="/">
+            UmairAsim<span className="text-[#3498DB] text-3xl">.</span>
+          </a>
         </div>
         <ul className="flex space-x-8">
           <li>
-            <a href="/" className="text-white hover:text-gray-300">
+            <a
+              href="/"
+              className="font-light text-white transition duration-200  hover:drop-shadow-[0_0_10px_#3498DB]"
+            >
               Home
             </a>
           </li>
           <li>
-            <a href="/about" className="text-white hover:text-gray-300">
+            <a
+              href="/about"
+              className="font-light text-white transition duration-200 ] hover:drop-shadow-[0_0_10px_#3498DB]"
+            >
               About
             </a>
           </li>
           <li>
-            <a href="/contact" className="text-white hover:text-gray-300">
+            <a
+              href="/contact"
+              className="font-light text-white transition duration-200  hover:drop-shadow-[0_0_10px_#3498DB]"
+            >
               Contact
             </a>
           </li>
